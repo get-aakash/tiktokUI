66900a6e9839231529a27a8ecaec87860ba2a478
Aakash Acharya <mail.aakash108@gmail.com>
2023-11-28 14:09:28 +1100

    loading multiple videos through API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import Video from './components/Video';
+import { useEffect, useState } from 'react';
 
 function App() {
+	const API_URL =
+	'https://raw.githubusercontent.com/codedamn-classrooms/tiktok-react-material/main/data.json'
+	const [videos, setVideos] = useState([])
+	useEffect(()=>{
+		fetch(API_URL)
+		.then((res)=>res.json())
+		.then((d)=>setVideos(d))
+
+	},[])
   return (
     <div className="app">
 			<div className="container">
-				<Video className="player" loop url="https://github.com/codedamn-classrooms/tiktok-react-material/raw/main/v1.mp4" />
+				{videos.map((video)=>{
+					return (
+						<Video className="player" loop url= {video.url}/>
+					)
+				})}
+				
 			</div>
 		</div>
   );

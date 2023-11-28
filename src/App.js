1fa235c2299b45e0497e46d9e26b725f3e19ee8d
Aakash Acharya <mail.aakash108@gmail.com>
2023-11-28 13:25:41 +1100

    adding player styles

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="app">
 			<div className="container">
-				<Video url="https://github.com/codedamn-classrooms/tiktok-react-material/raw/main/v1.mp4" />
+				<Video className="player" loop url="https://github.com/codedamn-classrooms/tiktok-react-material/raw/main/v1.mp4" />
 			</div>
 		</div>
   );
